c70d64060a4f133be52d49aae5345305b3a3c7ba
Akshay <anibrad@cyzma.com>
2023-07-13 17:14:14 +0530

    Filter chart

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,9 +3,22 @@ import React, { useState } from "react";
 import ExpenseFilter from "./ExpensesFilter";
 import ExpenseList from "./ExpensesList";
 import Card from "../UI/Card";
+import ExpensesChart from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2019");
+
+  let filterInfoText = "2020, 2021 & 2022";
+
+  if (filteredYear === "2020") {
+    filterInfoText = "2019, 2021 & 2022";
+  } else if (filteredYear === "2021") {
+    filterInfoText = "2019, 2020 & 2022";
+  } else if (filteredYear === "2022") {
+    filterInfoText = "2019, 2020 & 2021";
+  } else {
+    filterInfoText = "2020, 2021 & 2022";
+  }
 
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
@@ -22,6 +35,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        <p>Data for year {filterInfoText} is hidden.</p>
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpenseList items={filteredExpenses} />
       </Card>
     </div>
